030a5cc6fdce33f3c4861757d79a2bdb206c1278
samarthisgood69-code <samarthisgood69@gmail.com>
2026-06-11 10:30:43 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,6 @@
+#take the input from the user
+num=float(input ("Enter a number: "))
+#calculate the square root using the exponents
+result=num ** 0.5
+#display the output
+print(f"the square root of {num} is {result}")
